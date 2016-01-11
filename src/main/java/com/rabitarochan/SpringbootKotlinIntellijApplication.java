d04b022b2aee46080f2d4c79602c9f4d9fdf1d15
rabitarochan <rabitarochan@gmail.com>
2016-01-11 22:49:17 +0900

    Initial commit.

---
+++ src/main/java/com/rabitarochan/SpringbootKotlinIntellijApplication.java
@@ -0,0 +1,12 @@
+package com.rabitarochan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKotlinIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKotlinIntellijApplication.class, args);
+	}
+}
